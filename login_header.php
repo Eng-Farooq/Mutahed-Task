b74562290097b58356dfbbd84928230c7411274d
Eng-Farooq <107753135+Eng-Farooq@users.noreply.github.com>
2022-09-23 02:01:09 +0300

    Add files via upload

---
+++ login_header.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+  
+<head>
+  <title>مهامي - المجلس التنسيقي</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+  <link rel="stylesheet" href="assets/css/bootstrap.theme.min.css">
+  <link rel="stylesheet" href="assets/css/custom.css">
+  <link rel="icon" href="https://mutahed.org/assets/web/images/logo.png"/>
+  <style>
+    body{
+      background-image: url('https://i.imgur.com/TsyZbYJ.png');
+      background-repeat: no-repeat;
+      background-size: cover;
+    }
+    </style>
+  <script src="assets/js/jquery.min.js"></script>
+  <script src="assets/js/bootstrap.min.js"></script>
+  <script src="assets/js/custom.js"></script>
+</head>
+<body>
+
+
+
+
+
+
+<div class="main">
